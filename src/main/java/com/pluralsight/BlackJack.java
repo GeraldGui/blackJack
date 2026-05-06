c0367fe2848f69ea2f988ffa1b882f6ea6b6724c
Gerald G <geraldguignard2@gmail.com>
2026-05-06 15:38:06 -0400

    Finished the game to play BlackJack.

--- src/main/java/com/pluralsight/BlackJack.java
+++ src/main/java/com/pluralsight/BlackJack.java
@@ -7,12 +7,11 @@ public class BlackJack {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
         int target = 21;
-        boolean numIsClose;
 
-        System.out.println("Enter player 1 name: " );
+        System.out.print("Enter player 1 name: " );
         String player1name = myObj.nextLine();
 
-        System.out.println("Enter player 2 name: " );
+        System.out.print("Enter player 2 name: " );
         String player2name = myObj.nextLine();
 
         Deck deck = new Deck();
@@ -31,9 +30,16 @@ public class BlackJack {
         player1.print();
         player2.print();
 
+        int player1Score = player1.getValue();
+        int player2Score = player2.getValue();
 
-
-
-
+        if (player1Score <= target && (player1Score > player1Score || player2Score > target)) {
+            System.out.println(player1name + "Wins");
+        } else if (player2Score <= 21 && (player2Score > player1Score || player1Score > target)) {
+            System.out.println(player2name + "Wins");
+        } else {
+            System.out.println("It's a tie.");
+        }
+        myObj.close();
     }
 }
